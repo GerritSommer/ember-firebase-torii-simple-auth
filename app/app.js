d4c2085336070e19294978709098a063a1b25dfd
Gerrit Sommer <gerrit.sommer@outlook.com>
2017-03-17 10:42:20 +0100

    indent app.js

--- app/app.js
+++ app/app.js
@@ -1,15 +1,14 @@
-import Ember from 'ember';
-import Resolver from './resolver';
+import Ember            from 'ember';
+import Resolver         from './resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import config           from './config/environment';
 
-let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
+const App = Ember.Application.extend({
+  modulePrefix:     config.modulePrefix,
+  podModulePrefix:  config.podModulePrefix,
   Resolver
 });
 
