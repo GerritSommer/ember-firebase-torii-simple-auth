2ac80e966130f87ad4e7c42465be869d48031192
Gerrit Sommer <gerrit.sommer@outlook.com>
2017-03-17 10:41:46 +0100

    configure firebase and torii

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,14 @@
 /* eslint-env node */
 
+
+
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'contacts',
-    environment: environment,
-    rootURL: '/',
+    environment:  environment,
+    rootURL:      '/',
     locationType: 'auto',
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -20,26 +23,52 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    firebase: {
+      apiKey:             '[PLACE YOUR CONFIG HERE]',
+      authDomain:         '[PLACE YOUR CONFIG HERE]',
+      databaseURL:        '[PLACE YOUR CONFIG HERE]',
+      storageBucket:      '[PLACE YOUR CONFIG HERE]',
+      messagingSenderId:  '[PLACE YOUR CONFIG HERE]',
+    },
+
+    // 'ember-simple-auth': {
+    //   // routeAfterAuthentication: 'application',
+    //   authenticationRoute:      'login'
+    // },
+    //
+    torii: {
+      sessionServiceName: 'session'
+    },
+
+
+    contentSecurityPolicy: {
+      // 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com"
+      'connect-src':  "'self' wss://*.firebaseio.com https://*.googleapis.com",
+      'script-src':   "'self' 'unsafe-eval' apis.google.com",
+      'frame-src':    "'self' https://*.firebaseapp.com",
+      'font-src':     "'self' fonts.gstatic.com",
+      'style-src':    "'self' fonts.googleapis.com"
+    },
+
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_RESOLVER             = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION    = true;
+    // ENV.APP.LOG_TRANSITIONS          = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS         = true;
   }
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.locationType = 'none';
-
+    ENV.locationType              = 'none';
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.LOG_VIEW_LOOKUPS      = false;
+    ENV.APP.rootElement           = '#ember-testing';
   }
 
   if (environment === 'production') {
